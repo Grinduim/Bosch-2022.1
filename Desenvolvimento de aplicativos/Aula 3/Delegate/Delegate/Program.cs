f604e21118d49df77fb5afb1251bdd2eef6e4653
Vinicius <viniciusgribelperson@gmail.com>
2022-04-04 11:10:38 -0300

    upload arquivos

---
+++ Desenvolvimento de aplicativos/Aula 3/Delegate/Delegate/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// MeuDelegate md = Console.WriteLine;
+
+//public delegate void MeuDelegate(string texto); // delegate -> funções do tipo void que recebe uma string -> console.writeline
+// ou qualquer outro tipo de função q pode ser usada assim
+
+Func<int, bool> f = i => i < 5;
+
+int [] arr = {1, 2, 3, 4, 5, 6, 7,8,9,10,11,};
+
+var result = Where(arr, meuteste);
+
+foreach (int i in result)
+{
+    Console.WriteLine(i);
+}
+
+bool meuteste(int[] arr)
+{
+    return i > 5;
+}
+
+IEnumerable<int> Where(IEnumerable<int> coll, Func<int,bool> func)
+{
+    List<int> result = new List<int>();
+    foreach (int item in coll)
+    {
+        if (func(item))
+        {
+            result.Add(item);
+        }
+    }
+    return result;
+}
+
+public static 
---
+++ Desenvolvimento de aplicativos/Aula 3/LinQ/LinQ/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using static System.Console;
+
+Console.WriteLine("Hello, World!");
+
+MinhaColecao coll  = new MinhaColecao();
+
+coll.Add(8);
+coll.Add(10);
+
+WriteLine(coll.Get(1));
+
+public class MinhaColecao : IEnumerable<int>
+{
+    public List<int> lista = new List<int>();
+
+    public void Add(int i)
+    {
+        this.lista.Add(i);
+    }
+
+    public int Get( int index)
+    {
+        return this.lista[index];
+    }
+
+    public IEnumerator<int> GetEnumerator()
+    {
+        for(int i = 0; i < this.lista.Count; i++)
+        {
+            yield return this.lista[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+        => GetEnumerator();
+}
+
+public class MeuInteirador : IEnumerator<int>
+{
+    private MinhaColecao collection;
+    int i = -1;
+
+    public MeuInteirador(MinhaColecao coll)
+    {
+        this.collection = coll;
+    }
+    
+
+    public int Current => throw new NotImplementedException();
+
+    object IEnumerator.Current
+    {
+        get
+        {
+            return Current;
+        }
+    }
+
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool MoveNext()
+    {
+        i++;
+        if( i >= collection.lista.Count)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    public void Reset()
+    {
+        i = -1;
+    }
+}
