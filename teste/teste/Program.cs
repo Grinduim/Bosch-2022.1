b727b066ec924771d88d9cff07455d452262e223
Grinduim <viniciusgribelperson@gmail.com>
2022-04-26 22:23:43 -0300

    aa

---
+++ teste/teste/Program.cs
@@ -0,0 +1,8 @@
+﻿using teste.DAO;
+
+using (var context = new DAOContext())
+{
+   context.Database.EnsureCreated();
+} 
+
+// nesse momento eu deveria criar os metodos apra fazer os inserts e selects no banco de dados...
